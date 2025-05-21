121989632d61697aeffb94a047096141e6a448ee
Caba Nicolas <ncaba127@alumno.unlam.edu.ar>
2025-05-21 08:53:02 -0300

    Implementacion de ServicioTienda, Modificacion del TiendaController y modificacion de la vista tienda para mostrar articulos dinamicamente.

--- src/main/java/com/tallerwebi/controller/TiendaController.java
+++ src/main/java/com/tallerwebi/controller/TiendaController.java
@@ -1,16 +1,34 @@
 package com.tallerwebi.controller;
 
+import com.tallerwebi.service.ServicioTienda;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/tienda")
 public class TiendaController {
 
+    private final ServicioTienda servicioTienda;
+
+    @Autowired
+    public TiendaController(ServicioTienda servicioTienda) {
+        this.servicioTienda = servicioTienda;
+    }
+
     @GetMapping
-    public String cargarTienda() {
-        return "tienda";
+    public ModelAndView cargarTienda() {
+        ModelMap modelo = new ModelMap();
+
+        modelo.put("trampas", servicioTienda.obtenerTrampas());
+        modelo.put("vidas", servicioTienda.obtenerVidas());
+        modelo.put("monedas", servicioTienda.obtenerMonedas());
+        modelo.put("avatares", servicioTienda.obtenerAvatares());
+
+        return new ModelAndView("tienda", modelo);
     }
 
 }
