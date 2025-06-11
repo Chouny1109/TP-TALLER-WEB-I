617114bf58f2676db4afaab606655442ac3b29e0
Caba Nicolas <ncaba127@alumno.unlam.edu.ar>
2025-06-11 11:52:22 -0300

    Implementacion de imagenes de trampas y obtencion de cantidad de trampas por usuario permitiendo agregar.

--- src/main/java/com/tallerwebi/repository/RepositorioTrampas.java
+++
@@ -1,8 +0,0 @@
-package com.tallerwebi.repository;
-
-import com.tallerwebi.model.Trampa;
-import java.util.List;
-
-public interface RepositorioTrampas {
-    List<Trampa> buscarTrampasPorUsuario(Long idUsuario);
-}
