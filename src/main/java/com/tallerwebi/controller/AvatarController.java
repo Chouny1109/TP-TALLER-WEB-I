ea9bddfb2032c41f7f6e95e66db7d529249b2513
LUCIezq <lucieze02@icloud.com>
2025-07-10 18:45:20 -0300

    UPDATE: Corrigo datos harcodeados

--- src/main/java/com/tallerwebi/controller/AvatarController.java
+++ src/main/java/com/tallerwebi/controller/AvatarController.java
@@ -1,18 +1,40 @@
 package com.tallerwebi.controller;
 
+import com.tallerwebi.model.Usuario;
+import com.tallerwebi.util.SessionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/avatar")
 public class AvatarController {
 
+    private final SessionUtil sessionUtil;
+
+    @Autowired
+    public AvatarController(SessionUtil sessionUtil) {
+        this.sessionUtil = sessionUtil;
+    }
+
     @GetMapping
-    public String cargarAvatar() {
-        return "avatar";
+    public ModelAndView cargarAvatar(HttpServletRequest request) {
+        ModelMap model = new ModelMap();
+        Usuario logueado = sessionUtil.getUsuarioLogueado(request);
+
+        if (logueado == null) {
+            return new ModelAndView("redirect:/login");
+        }
+
+        model.addAttribute("usuario", logueado);
+        return new ModelAndView("avatar", model);
     }
 
     @PostMapping
