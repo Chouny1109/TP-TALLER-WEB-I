41b74ac67610ba206138ed9826bfde96ef32b7db
LUCIezq <lucieze02@icloud.com>
2025-07-16 14:52:04 -0300

    UPDATE: Nivel en vista cargar partida

--- src/main/java/com/tallerwebi/service/impl/ServicioNivelImpl.java
+++ src/main/java/com/tallerwebi/service/impl/ServicioNivelImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class ServicioNivelImpl implements ServicioNivel {
 
     private final RepositorioNivel repositorioNivel;
